f217b0c913bad8a1ee11d6bc4118a77cc55666df
Andrei B <andreibarut@gmail.com>
2023-02-23 19:27:05 +0200

    added stop vote functionality

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,13 +1,30 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { db } from "../firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Results = () => {
 	const pollId = useParams();
+	const [userName, setUserName] = useState(null);
+	const [userId, setUserId] = useState(null);
 	const [resultsData, setResultsData] = useState(null);
 	const [currentDocId, setCurrentDocId] = useState(null);
 	const [isLoaded, setIsLoaded] = useState(false);
+
+	//^GETTING CURRENT USER AND USER ID
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				setUserName(user.displayName);
+				setUserId(user.uid);
+			} else {
+				//if there is no logged in user, redirect to home page
+				navigateTo("/");
+			}
+		});
+	}, []);
 
 	//^GETTING FIRESTORE POLL BASED ON USE PARAMS()S
 	useEffect(() => {
@@ -30,6 +47,34 @@ const Results = () => {
 		console.log(resultsData);
 		getDocument();
 	}, []);
+
+	const handleStopVote = (e) => {
+		e.preventDefault();
+		e.target.disabled = true;
+		async function updateStopVote() {
+			const docRefRes = doc(db, "polls", currentDocId);
+			await updateDoc(docRefRes, {
+				"data.active": false,
+			});
+			console.log("document updated with ID: ", "/polls/", currentDocId);
+			// navigateTo(`/results/${pollId}`);
+			window.location.reload();
+		}
+
+		if (resultsData.data.voteType === "private") {
+			console.log("not sending name alongside with answers");
+			//here we will have an array of as many maps as the options are
+			//the maps will have the key (optionName) and the value as a number which is incremented as users vote.
+		} else {
+			console.log("sending reults alongside with name");
+			//here we will send an array which contains as many maps as the options are
+			//the maps will have the key (optionName), and the value as an array which is modified (usernames pushed in it) as users post (using updateDoc())
+		}
+		//after we update the doc, we will navigate to a new page called pollResults, which will show the results.
+		//on the results page, if the user is the creator of that specific poll, we verify using pollId, then we show a button which says stop vote. This will update a field (which does not exist yet) "open" = false
+		//when open is false then Vote page will not shot options to vote, instead it will show the text problem, and a button which when clicked will take the user to the Results page.
+		updateStopVote();
+	};
 
 	return (
 		<article className="results-article-container">
@@ -54,6 +99,13 @@ const Results = () => {
 								</span>
 							</p>
 						))}
+
+						{resultsData.data.active && resultsData.data.pollAuthorId === userId && (
+							<button onClick={handleStopVote}>STOP VOT</button>
+						)}
+						{!resultsData.data.active && (
+							<p style={{ color: "red" }}>Votarea a fost închisă de către autor!</p>
+						)}
 					</div>
 				</div>
 			)}
